970004651368412cdd0e6044551e1d1ab992088a
0MollyMan0 <fougereantoine0pro@gmail.com>
2025-11-08 14:28:12 +0100

    ulstr

--- level_01/ulstr.c
+++ level_01/ulstr.c
@@ -6,19 +6,34 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 13:46:41 by anfouger          #+#    #+#             */
-/*   Updated: 2025/11/08 13:49:18 by anfouger         ###   ########.fr       */
+/*   Updated: 2025/11/08 14:27:35 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	reverse_case(const char *s)
+{
+	int i;
+	char c;
+	
+	i = 0;
+	while (s[i])
+	{
+		c = s[i];
+		if (c >= 'a' && c <= 'z')
+			c -= 32;
+		else if (c >= 'A' && c <= 'Z')
+			c += 32;
+		write(1, &c, 1);
+		i++;
+	}
+}
+
 int main(int argc, char const *argv[])
 {
-	if (argc != 2)
-	{
-		write(1, "\n", 1);
-		return (0);
-	}
-	
+	if (argc == 2)
+		reverse_case(argv[1]);
+	write(1, "\n", 1);
 	return (0);
 }
