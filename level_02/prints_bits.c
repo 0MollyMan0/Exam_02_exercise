84c610900c888a679c0617b4f323001a2270b3df
0MollyMan0 <fougereantoine0pro@gmail.com>
2025-11-12 19:10:00 +0100

    print_bits

--- level_02/prints_bits.c
+++ level_02/prints_bits.c
@@ -10,7 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void	print_bits(unsigned char octet)
+#include <unistd.h>
+
+void print_bits(unsigned char octet)
 {
-	
+	int i;
+	unsigned char bit;
+
+	i = 8;
+	while (i--)
+	{
+		bit = (octet >> i & 1) + 48;
+		write(1, &bit, 1);
+	}
 }
